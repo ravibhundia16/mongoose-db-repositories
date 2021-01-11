bac92e90787f15d2aaca6ec878ea52bd7122e4bd
Ravi Bhundia <ravi.bhundia@scikey.ai>
2021-01-11 12:19:49 +0530

    reactor: update common repo export module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const models = require('@ravibhundia/mongoose-db-schema')
 
 module.exports = {
-  commonRepository = require('./repository/common-repository')
+  commonRepository: require('./repository/common-repository')
 }
